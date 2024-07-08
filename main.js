a900544e1012d677c7c40f813b6438889d6da3a5
Leonardo Reyes <leoreyes_07@outlook.com>
2024-07-08 08:04:59 -0600

    Merged shopping cart adn created open and close menus

--- main.js
+++ main.js
@@ -2,14 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const iconCartMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail'); 
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+iconCartMenu.addEventListener('click', toggleAsideCart);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive'); 
+    }
+
+
     desktopMenu.classList.toggle('inactive');
 }
+
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive'); 
+    }
+
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleAsideCart() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    //aside.classList.toggle('inactive');
+
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive'); 
+    }
+
+    aside.classList.toggle('inactive');    
+}   
